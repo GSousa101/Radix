1b4e8fd08ad2941e47577cdfe576295e7827f0fb
gusousa <gfsousa42@gmail.com>
2022-11-05 03:38:34 -0300

    initial commit

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+# define MAIN_H
+
+//pos_y, pos_x = cordenadas do objeto
+typedef struct s_navio {
+    int pos_y;
+    int pos_x;
+}   t_navio;
+
+typedef struct s_unidade {
+    int pos_y;
+    int pos_x;
+}   t_navio;
+
+#endif
